aba45b69c704e1ac87bf368b83dda585fc738583
Diego Pinto <dicapisar@gmail.com>
2021-01-28 07:58:53 -0500

    Creación de API para operaciones CRUD en base de datos MYSQL

--- Server/src/controllers/gamesController.ts
+++ Server/src/controllers/gamesController.ts
@@ -2,9 +2,35 @@ import {Request, Response } from 'express'
 import pool from '../database'
 
 class GamesController {
-    public index (req: Request, res:Response) {
-        pool.query('DESCRIBE gams')
-        res.json('games');
+    public async list (req: Request, res:Response) {
+        const games = await pool.query('SELECT * FROM games');
+        res.json(games)
+    }
+
+    public async getOne (req: Request, res:Response): Promise<any> {
+        const { id } = req.params;
+        const game = await pool.query('SELECT * FROM games WHERE id = ?', [id]);
+        if(game.length > 0){
+            return res.json(game[0]);
+        }       
+        res.status(404).json({text: "Juego no encontrado"});
+    }
+
+    public async create (req: Request, res: Response): Promise<void> {
+        await pool.query('INSERT INTO games set ?', [req.body])
+        res.json({message: 'Juego Guardado'})
+    }
+
+    public async delete (req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        await pool.query('DELETE FROM games WHERE id = ?', [id]);
+        res.json({message: "El juego fue eliminado"})
+    }
+
+    public async update (req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        await pool.query('UPDATE games set ? WHERE id = ?', [req.body, id]);
+        res.json({message: "El juego fue Actualizado"});
     }
 }
 
